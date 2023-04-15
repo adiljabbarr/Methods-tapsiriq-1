32d7fbaa28df38250072f6ee3ff118348b6fb59a
adilj <adilj@DESKTOP-VPK29AF>
2023-04-15 22:28:47 +0400

    Add project files.

---
+++ Methods 1/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Methods_1
+{
+    internal class Program
+    {
+        #region task1
+        //static void Main(string[] args)
+        //{
+        //    int.TryParse(Console.ReadLine(), out var num1);
+        //    int.TryParse(Console.ReadLine(), out var num2);
+        //    sum(num1, num2);
+        //}
+
+        //public static int sum(int num1 , int num2)
+        //{
+        //    int sum = num1 + num2;
+        //    Console.WriteLine("ededlerin cemi:" + sum);
+        //    return sum;
+        //}
+        #endregion
+
+        #region task2
+        //static void Main(string[] args)
+        //{
+        //    int[] arr = { 1, 2, 3 };
+        //    int ededlerin_ortasi;
+
+        //    ededi_orta(arr );
+        //}
+        //public static int ededi_orta(int[] arr)
+        //{
+        //    int ededlerin_ortasi;
+        //    int sum = 0;
+
+        //    foreach (var item in arr)
+        //    {
+        //        sum += item;
+        //    }
+        //    ededlerin_ortasi = sum / arr.Length;
+        //    Console.WriteLine("ededlerin ortasi:" + ededlerin_ortasi);
+        //    return ededlerin_ortasi;
+        //}       
+        #endregion
+
+        #region task3 
+        //static void Main(string[] args)
+        //{
+        //    float.TryParse(Console.ReadLine(), out var num);
+        //    RoundedNum(num);
+        //}
+
+        //private static void  RoundedNum(float num)
+        //{
+        //    Console.WriteLine(Math.Round(num));
+        //}
+        #endregion
+
+        #region task4
+        //static void Main(string[] args)
+        //{           
+        //    Console.WriteLine("enter your num");
+        //    int.TryParse(Console.ReadLine(), out var num);
+        //    OddOrEvenNum(num);
+        //}
+        //private static bool OddOrEvenNum(int num)
+        //{
+        //    if (num % 2==0 && num !=0)
+        //    {
+        //        Console.WriteLine("number is even");               
+        //    }
+        //    else if (num == 0) 
+        //    {
+        //        Console.WriteLine("edediniz 0-dir ");                
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("edediniz tek ededdir");
+        //    }
+        //    return true;
+        //}
+        #endregion
+
+        #region task5
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("ders oxumuz ?");
+        //    bool a = true;
+        //    Lesson(a);
+        //}
+        //public static string Lesson(bool a)
+        //{
+        //    if (a == true)
+        //    {
+        //        Console.WriteLine("aferim , derse hazirlasmisiz");
+        //        return "dersi oxumusuz ,aferim";
+        //    }
+        //    else
+        //    {
+        //        return "ayib olsun , derse hazirlasmamisiz ";
+        //    }
+        //}
+        #endregion
+
+        #region task6
+        //static void Main(string[] args)
+        //{           
+        //    Console.WriteLine("Please enter your Name");
+        //    string name = Console.ReadLine();
+        //    Console.WriteLine("Please enter your Surname");
+        //    string surname = Console.ReadLine();
+        //    fullname(name , surname);
+        //}
+        //private static void fullname(string name , string surname)
+        //{
+        //    Console.WriteLine($"sizin adiniz {name} ve soyadiniz {surname}");
+        //}
+        #endregion
+
+        #region task7
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("enter number ");
+        //    decimal.TryParse(Console.ReadLine(), out var num);
+        //    SquareofNumber(num);
+        //}
+        //public static decimal SquareofNumber(decimal num)
+        //{
+        //    return num*num;
+        //}
+        #endregion
+
+        #region task9
+        //static void Main(string[] args)
+        //{
+        //    Console.Write("enter number:");
+        //    int.TryParse(Console.ReadLine(), out var num);
+        //    Console.Write("enter power:");
+        //    int.TryParse(Console.ReadLine(), out var power);
+        //    Console.Write("Result:");
+        //    Result(num ,power);
+        //}
+        //public static void Result(int num ,int power)
+        //{            
+        //    double  sum = Math.Pow(num,power);
+        //    Console.WriteLine(sum);          
+        //}
+        #endregion
+    }
+}
